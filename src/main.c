de7b96c2c52cfd96ba112b2cc177ca1279f64d85
Connor Garey <connorgarey1999@gmail.com>
2025-05-08 20:46:51 +0100

    Added pragma once to the header.

--- src/main.c
+++ src/main.c
@@ -9,7 +9,5 @@ int main(int argc, char *argv[])
   const char note_names_sharp[12][3] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
   const char note_names_flat[12][3] = {"C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"};
   
-  // print_scale(struct MusicKey *key)
-  print_scale();
   return EXIT_SUCCESS;
 }
