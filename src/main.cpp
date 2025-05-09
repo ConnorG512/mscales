d5827ec5a82b883cd5a9f975a70e67cb7aec5b53
Connor Garey <connorgarey1999@gmail.com>
2025-05-09 17:34:26 +0100

    Function for printing the tonic note as well as the scale in the title bar.

--- src/main.cpp
+++ src/main.cpp
@@ -16,5 +16,7 @@
 int main (int argc, char *argv[]) 
 {
   auto c_major { std::make_unique<MusicScale>(MusicScale::TonicNote::C, MusicScale::ScaleType::Major )};
+  c_major->printTitlebar();
+  c_major->calculateMusicKey();
   return 0;
 }   
--- src/scale.cpp
+++ src/scale.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ostream>
 #include <string>
+#include <format>
 
 #include "scale.hpp"
 
@@ -15,51 +16,70 @@ void MusicScale::printTitlebar()
   switch (m_tonic) {
     case MusicScale::TonicNote::C:
       tonic_note = "C";
+      break;
     case MusicScale::TonicNote::C_sharp:
       tonic_note = "C#";
+      break;
     case MusicScale::TonicNote::D_flat:
       tonic_note = "Db";
+      break;
     case MusicScale::TonicNote::D:
       tonic_note = "D";
+      break;
     case MusicScale::TonicNote::D_sharp:
       tonic_note = "D#";
+      break;
     case MusicScale::TonicNote::E_flat:
       tonic_note = "Eb";
+      break;
     case MusicScale::TonicNote::E:
       tonic_note = "E";
+      break;
     case MusicScale::TonicNote::F:
       tonic_note = "F";
+      break;
     case MusicScale::TonicNote::F_sharp:
       tonic_note = "F#";
+      break;
     case MusicScale::TonicNote::G_flat:
       tonic_note = "Gb";
+      break;
     case MusicScale::TonicNote::G:
       tonic_note = "G";
+      break;
     case MusicScale::TonicNote::G_sharp:
       tonic_note = "G#";
+      break;
     case MusicScale::TonicNote::A_flat:
       tonic_note = "Ab";
+      break;
     case MusicScale::TonicNote::A:
       tonic_note = "A";
+      break;
     case MusicScale::TonicNote::A_sharp:
       tonic_note = "A#";
+      break;
     case MusicScale::TonicNote::B_flat:
       tonic_note = "Bb";
+      break;
     case MusicScale::TonicNote::B:
       tonic_note = "B";
+      break;
   }
   switch (m_scale) {
     case MusicScale::ScaleType::Major:
       scale = "Major";
+      break;
     case MusicScale::ScaleType::Minor:
-      scale = "Minor";
+      scale = "Minor"; 
+      break;
   }
-  
+  std::cout << std::format("{} {}:", tonic_note, scale) << std::endl;
 }
 void MusicScale::calculateMusicKey()
 {
   // TODO
-  std::cout << "calculateMusicKey called!" << std::endl;
+  std::cout << "TODO: calculateMusicKey called!" << std::endl;
 
   //  std::cout << "scale_increments size: " << scale_increments.size() << std::endl;
   // while (scale_position <= scale_increments.size()) {
