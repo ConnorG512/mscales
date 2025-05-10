3791178b986b7aa927c526630bc763e796815789
Connor Garey <connorgarey1999@gmail.com>
2025-05-10 19:18:11 +0100

    Main will now print in multiple Major keys.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,17 @@
 
 int main (int argc, char *argv[]) 
 {
-  auto major_scale { std::make_unique<MusicScale>(MusicScale::TonicNote::D) };
+  auto major_scale { std::make_unique<MusicScale>(MusicScale::TonicNote::C) };
+  major_scale->startKeyGeneration();
+  major_scale->m_key_tonic = MusicScale::TonicNote::G;
+  major_scale->startKeyGeneration();
+  major_scale->m_key_tonic = MusicScale::TonicNote::D;
+  major_scale->startKeyGeneration();
+  major_scale->m_key_tonic = MusicScale::TonicNote::A;
+  major_scale->startKeyGeneration();
+  major_scale->m_key_tonic = MusicScale::TonicNote::E;
+  major_scale->startKeyGeneration();
+  major_scale->m_key_tonic = MusicScale::TonicNote::B;
   major_scale->startKeyGeneration();
   return 0;
 }   
