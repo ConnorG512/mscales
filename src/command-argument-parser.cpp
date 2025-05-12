6719ef1fc1b6945ea05b4bfa35ee35e07dee10ba
Connor Garey <connorgarey1999@gmail.com>
2025-05-12 13:21:46 +0100

    Added stubs

--- src/command-argument-parser.cpp
+++ src/command-argument-parser.cpp
@@ -1,15 +1,14 @@
-#include <array>
-#include <cstdint>
+#include <iostream>
+#include <ostream>
 
 #include "command-argument-parser.hpp"
 
 bool CommandParser::parseStringArgument(std::string_view& command)
 {
-  static constexpr std::array<std::string_view, 2> command_arguments{
-    "--c-major", "--c#-major"};
-
-  for (uint8_t index {0}; index < command_arguments.size(); ++index ) {
-     
-  } 
-  return false;
+  std::cout << "Stub:: parseStringArguement called!" << std::endl;
+  return true;
+}
+void CommandParser::countArgumentNum(uint8_t argument_num)
+{
+  std::cout << "Stub:: countArgumentNum called!" << std::endl;
 }
