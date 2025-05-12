63bc53951d73e3da3dd3c2e540fe6f56db9ded69
Connor Garey <connorgarey1999@gmail.com>
2025-05-12 13:23:03 +0100

    command arguement functionality that will be replaced later with the class.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "scale.hpp"
+#include <array>
+#include <cstdint>
 #include <iostream>
 #include <memory>
 #include <ostream>
 #include <stdlib.h>
+#include <string_view>
 #include <strings.h>
 
 int main (int argc, char *argv[]) 
@@ -12,7 +15,18 @@ int main (int argc, char *argv[])
     std::cout << "No Arguements provided!" << std::endl;
     return 0; 
   }
-
+  constexpr static std::array<std::string_view, 2> command_arguements{"--c-major", "--c#-major"};
+  for (uint8_t index { 0 }; index < command_arguements.size(); ++index) {
+    if (command_arguements[ index ].compare("--c-major") == 0) {
+      auto major_scale {std::make_unique<MusicScale>(MusicScale::TonicNote::C)};
+      major_scale->startKeyGeneration();
+    } 
+    else if (command_arguements[ index ].compare("--c#-major") == 0) {
+      auto major_scale {std::make_unique<MusicScale>(MusicScale::TonicNote::C_Sharp)};
+      major_scale->startKeyGeneration();
+    }
+    return 0;
+  }
 
   std::cout << "Major Keys:" << std::endl;
   auto major_scale { std::make_unique<MusicScale>(MusicScale::TonicNote::G_Flat) };
